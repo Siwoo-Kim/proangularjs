a730f8d6f0d8f62a7017ab3922b61c42ffadc4e5
SiWoo Kim <35586824+Siwoo-Kim@users.noreply.github.com>
2018-07-30 11:56:30 -0700

    first commit - chapter1~2

---
+++ src/main/java/com/proangularjs/swangularjs/SwAngularjsApplication.java
@@ -0,0 +1,12 @@
+package com.proangularjs.swangularjs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SwAngularjsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SwAngularjsApplication.class, args);
+    }
+}
